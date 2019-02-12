56f4cbf9aafe4b8b9c7c15150fc58d92e52fad65
Matt Zuckermann <mczuckermann@gmail.com>
2019-02-12 14:42:54 -0800

    Added if/else statement that checks that all forms filled; alert given if not

--- public/js/scraper.js
+++ public/js/scraper.js
@@ -1,27 +1,36 @@
 $(`#saveNote`).on(`click`, function() {
-  const id = $(this).attr(`data-id`);
-  $.ajax({
-    method: `POST`,
-    url: `/saveComment/${id}`,
-    data: {
-      subject: $(`#subjectForm`)
-        .val()
-        .trim(),
-      author: $(`#authorForm`)
-        .val()
-        .trim(),
-      body: $(`#bodyForm`)
-        .val()
-        .trim(),
-    },
-  })
-    // Return json with updated note to scraped article
-    .then(function(dataScrape) {
-      console.log(dataScrape);
-      $(`#notesDiv`).empty();
-    });
+  if (
+    $(`#subjectForm`).val() === `` ||
+    $(`#authorForm`).val() === `` ||
+    $(`#bodyForm`).val() === ``
+  ) {
+    // eslint-disable-next-line no-alert
+    alert(`Please fill out all the forms!`);
+  } else {
+    const id = $(this).attr(`data-id`);
+    $.ajax({
+      method: `POST`,
+      url: `/saveComment/${id}`,
+      data: {
+        subject: $(`#subjectForm`)
+          .val()
+          .trim(),
+        author: $(`#authorForm`)
+          .val()
+          .trim(),
+        body: $(`#bodyForm`)
+          .val()
+          .trim(),
+      },
+    })
+      // Return json with updated note to scraped article
+      .then(function(dataScrape) {
+        console.log(dataScrape);
+        $(`#notesDiv`).empty();
+      });
 
-  $(`#subjectForm`).val(``);
-  $(`#authorForm`).val(``);
-  $(`#bodyForm`).val(``);
+    $(`#subjectForm`).val(``);
+    $(`#authorForm`).val(``);
+    $(`#bodyForm`).val(``);
+  }
 });
